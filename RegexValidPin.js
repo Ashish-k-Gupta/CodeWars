ac60c2987ba5f74b46d9718c6a92f0a711689dab
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2023-09-28 21:45:53 +0530

    solved Regex Valid PIN code problem

---
+++ RegexValidPin.js
@@ -0,0 +1,18 @@
+/* 
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+
+Examples (Input --> Output)
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+*/
+
+// Solution
+
+function validatePIN(pin) {
+    // Use a regular expression to match 4 or 6 digit PIN codes
+    const pinPattern = /^(?:\d{4}|\d{6})$/;
+    return pinPattern.test(pin);
+}
